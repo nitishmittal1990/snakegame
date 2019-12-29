f3c29124873a29f913f850a68bb4fdc24b34d183
nitish mittal <nitish.mittal@orchids.edu.in>
2019-12-29 14:33:40 +0530

    Updates

---
+++ precache-manifest.c589e8e82e4c1289324ff21d900c27a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20ce25b46d6260db3495abfa3765ce0",
+    "url": "/snakegame/index.html"
+  },
+  {
+    "revision": "4d6c7e9306b9952f5f11",
+    "url": "/snakegame/static/css/main.f3c720f5.chunk.css"
+  },
+  {
+    "revision": "41cb1e145f737552776f",
+    "url": "/snakegame/static/js/2.15aeded2.chunk.js"
+  },
+  {
+    "revision": "4d6c7e9306b9952f5f11",
+    "url": "/snakegame/static/js/main.0e6d5622.chunk.js"
+  },
+  {
+    "revision": "00fae53cc04cbfff44b2",
+    "url": "/snakegame/static/js/runtime-main.5727376a.js"
+  }
+]);
